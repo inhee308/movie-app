87ae0eaeefa23063c7c1fd5b5a9b639a7957255b
inhee <inhee308@gmail.com>
2024-09-20 19:43:36 +0900

    영화 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes,Route} from 'react-router-dom';
+import Home from './routs/Home';
+import About from './routs/About';
+import Movies from './routs/Movies';
+import Navbar from "./components/Navbar";
+import './App.scss';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about:id" element={<About />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 }
